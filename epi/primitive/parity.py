52e4dbd0332c925fb12b217c7b8915b5a0958a28
rituc <ritu11choudhary@gmail.com>
2016-08-21 23:45:09 +0530

    parity

--- epi/primitive/parity.py
+++ epi/primitive/parity.py
@@ -1,7 +1,20 @@
 def main():
 	print "Enter number"
 	num = long(input())
-	print num
+	print "Even" if find_parity(num) == 0 else "Odd"
+	# test()
+
+
+def find_parity(num):
+	return 0 if (num & 1) == 0 else 1
+
+
+def test():
+	assert find_parity(0) == 0
+	assert find_parity(1) == 1
+	assert find_parity(110) == 0
+	assert find_parity(1000) == 0
+
 
 if __name__ == "__main__":
 	main()
