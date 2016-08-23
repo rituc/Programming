0160bc3f32d4f0df60f6d2ab2cf401fa903cf184
rituc <ritu11choudhary@gmail.com>
2016-08-23 23:57:39 +0530

    count set bits

--- epi/primitive/parity.py
+++ epi/primitive/parity.py
@@ -1,12 +1,21 @@
 def main():
 	print "Enter number"
 	num = long(input())
-	print "Even" if find_parity(num) == 0 else "Odd"
+	print count_set_bits(num)
+	# print "Even" if find_parity(num) == 0 else "Odd"
 	# test()
 
 
 def find_parity(num):
 	return 0 if (num & 1) == 0 else 1
+
+def count_set_bits(num):
+	set_count = 0
+	while num > 0:
+		if(num & 1 == 1):
+			set_count +=1
+		num = num >> 1
+	return set_count
 
 
 def test():
