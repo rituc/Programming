8bb131219ce7b6b03f5b68d67ce21ebc99747bf9
rituc <ritu11choudhary@gmail.com>
2016-09-04 23:50:16 +0530

    code monk bits algo: monk_choice_of_num

--- code_monk/bit_algo/count_ones.py
+++ code_monk/bit_algo/count_ones.py
@@ -2,15 +2,11 @@ def main():
 	num = raw_input("Enter num")
 	print count_one(num)
 
-int count_one (int n)
-  {
-      while( n )
-      {
-      n = n&(n-1);
-         count++;
-      }
-      return count;
-}
+def count_one (int n)
+  while( n ):
+  n = n&(n-1);
+     count++;
+  return count;
 
 if __name__ == "__main__":
 	main()
---
+++ code_monk/bit_algo/monk_choice_of_num.py
@@ -0,0 +1,24 @@
+# https://www.hackerearth.com/code-monk-bit-manipulation/algorithm/monks-choice-of-numbers-1/editorial/
+
+def main():
+	T = int(raw_input("Enter number of test cases: "))
+	for t in range(T):
+		max = 0
+		N = int(raw_input("Enter list size: "))
+		for i in range(N):
+			one_counts = count_ones(int(raw_input("Enter number: ")))
+			if max < one_counts:
+				max = one_counts
+		print one_counts
+
+def count_one (int n)
+  while(n):
+  n = n&(n-1);
+     count++;
+  return count;
+
+if __name__ == "__main__":
+	main()
+
+
+
